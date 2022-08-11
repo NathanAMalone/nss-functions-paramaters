7587aaed51e88f2dd49655825c70d53a7690f19f
Nathan Malone <nathanamalone@gmail.com>
2022-08-11 14:16:43 -0500

    Functions Paramaters complete

---
+++ main.js
@@ -0,0 +1,10 @@
+// define parameter; divide by 4; return the number; save to var; console.log; 
+const quarterValue = (number) => {
+    const quarter = number/4
+    return quarter
+}
+// Invoke the function and store the return value
+const numFourth = quarterValue(10)
+
+// Log the return value
+console.log(numFourth)
